bfb9d62c3baeb3537200534ad54527a3aa7bdefb
jbockerstette <jim.bockerstette@gmail.com>
2018-06-19 16:12:29 -0700

    All but the bonuses are passing.

--- src/MarkDownPreviewer.js
+++ src/MarkDownPreviewer.js
@@ -1,15 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import marked from 'marked';
 import './MarkDownPreviewer.css';
+
+// Need to add this so that the test suite freecodecamp uses will not fail the tests.
+window.marked = marked;
+
+const defaultMD =
+  "# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here's some other cool stuff:\n  \nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == '```' && lastLine == '```') {\n    return multiLineCode;\n  }\n}\n```\n  \nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere's also [links](https://www.freecodecamp.com), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | ------------- \nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbererd lists too.\n1. Use just 1s if you want! \n1. But the list goes on...\n- Even if you use dashes or asterisks.\n* And last but not least, let's not forget embedded images:\n\n![React Logo w/ Text](https://goo.gl/Umyytc)\n";
 
 const Grid = props => <div className="grid">{props.children}</div>;
 
-const Previewer = props => <div id="preview">{props.text}</div>;
+class Previewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.markupRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.setMarkup();
+  }
+
+  componentDidUpdate() {
+    this.setMarkup();
+  }
+
+  setMarkup() {
+    // console.log(marked(this.props.text));
+    this.markupRef.current.innerHTML = marked(this.props.text);
+  }
+
+  render() {
+    return <div ref={this.markupRef} id="preview" />;
+  }
+}
 
 class Editor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = { text: props.defaultText };
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -22,6 +51,7 @@ class Editor extends React.Component {
   }
 
   render() {
+    const { text } = this.state;
     return (
       <textarea
         onChange={this.handleChange}
@@ -29,24 +59,26 @@ class Editor extends React.Component {
         id="editor"
         cols="30"
         rows="10"
-        value={this.state.text}
+        value={text}
       />
     );
   }
 }
 
 Editor.propTypes = {
-  onChange: PropTypes.func
+  onChange: PropTypes.func,
+  defaultText: PropTypes.string
 };
 
 Editor.defaultProps = {
-  onChange: () => {}
+  onChange: () => {},
+  defaultText: ''
 };
 
 class MarkDownPreviewer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { text: '' };
+    this.state = { text: props.defaultText };
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -59,11 +91,20 @@ class MarkDownPreviewer extends React.Component {
   render() {
     return (
       <Grid>
-        <Editor onChange={this.handleChange} />
+        <Editor
+          defaultText={this.props.defaultText}
+          onChange={this.handleChange}
+        />
         <Previewer text={this.state.text} />
       </Grid>
     );
   }
 }
+MarkDownPreviewer.propTypes = {
+  defaultText: PropTypes.string
+};
+MarkDownPreviewer.defaultProps = {
+  defaultText: defaultMD
+};
 
 export default MarkDownPreviewer;
