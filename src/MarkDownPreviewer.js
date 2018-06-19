1868dde8f711e5e4c58ec851198319baf14eb23c
jbockerstette <jim.bockerstette@gmail.com>
2018-06-19 12:51:31 -0700

    Started on my mark down.

--- src/MarkDownPreviewer.js
+++ src/MarkDownPreviewer.js
@@ -1,5 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './MarkDownPreviewer.css';
 
-const MarkDownPreviewer = () => <div>MarkDownPreviewer</div>;
+const Grid = props => <div className="grid">{props.children}</div>;
+
+const Previewer = props => <div id="preview">{props.text}</div>;
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { value } = e.target;
+    this.props.onChange(value);
+    this.setState(() => ({
+      text: value
+    }));
+  }
+
+  render() {
+    return (
+      <textarea
+        onChange={this.handleChange}
+        name="editor"
+        id="editor"
+        cols="30"
+        rows="10"
+        value={this.state.text}
+      />
+    );
+  }
+}
+
+Editor.propTypes = {
+  onChange: PropTypes.func
+};
+
+Editor.defaultProps = {
+  onChange: () => {}
+};
+
+class MarkDownPreviewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(value) {
+    this.setState(() => ({
+      text: value
+    }));
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Editor onChange={this.handleChange} />
+        <Previewer text={this.state.text} />
+      </Grid>
+    );
+  }
+}
 
 export default MarkDownPreviewer;
