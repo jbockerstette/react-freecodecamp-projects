feaec6a13418190eed39ce6a83b1419098329876
jbockerstette <jim.bockerstette@gmail.com>
2018-06-29 09:34:21 -0700

    Up down session and break buttons are working.

--- src/pomodoro-clock/PomodoroClock.js
+++ src/pomodoro-clock/PomodoroClock.js
@@ -1,30 +1,74 @@
 import React from 'react';
 import './PomodoroClock.css';
 
+const Button = ({ btnId, ...rest }) => <button id={btnId} {...rest} />;
+
+const PlusButton = props => (
+  <Button {...props}>
+    <i className="fa fa-plus" />
+  </Button>
+);
+const MinusButton = props => (
+  <Button {...props}>
+    <i className="fa fa-minus" />
+  </Button>
+);
+
+const UpDownControl = ({
+  onClickUp,
+  onClickDown,
+  valueId,
+  btnUpId,
+  btnDownId,
+  value
+}) => (
+  <div>
+    <PlusButton onClick={onClickUp} btnId={btnUpId} />
+    <span id={valueId}>{value}</span>
+    <MinusButton onClick={onClickDown} btnId={btnDownId} />
+  </div>
+);
+
 class PomodoroClock extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    this.handleBreakDecrement = this.handleBreakDecrement.bind(this);
-    this.handleBreakIncrement = this.handleBreakIncrement.bind(this);
-    this.handleSessionDecrement = this.handleSessionDecrement.bind(this);
-    this.handleSessionIncrement = this.handleSessionIncrement.bind(this);
+    this.state = { sessionMinutes: 25, breakMinutes: 5 };
   }
 
-  handleBreakDecrement() {}
-
-  handleBreakIncrement() {}
-
-  handleSessionDecrement() {}
-
-  handleSessionIncrement() {}
+  handleIncDec = (inc, min, max, propertyName) => () => {
+    this.setState(({ [propertyName]: prev }) => {
+      let next = prev + inc;
+      if (next < min) {
+        next = min;
+      } else if (next > max) {
+        next = max;
+      }
+      return { [propertyName]: next };
+    });
+  };
 
   render() {
+    const { sessionMinutes, breakMinutes } = this.state;
     return (
       <div>
-        <button onClick={this.handleBreakDecrement} id="break-decrement">
-          Hi
-        </button>
+        <div>Session</div>
+        <UpDownControl
+          btnDownId="session-decrement"
+          onClickDown={this.handleIncDec(-1, 1, 60, 'sessionMinutes')}
+          btnUpId="session-increment"
+          onClickUp={this.handleIncDec(1, 1, 60, 'sessionMinutes')}
+          valueId="session-length"
+          value={sessionMinutes}
+        />
+        <div>Break</div>
+        <UpDownControl
+          btnDownId="break-decrement"
+          onClickDown={this.handleIncDec(-1, 1, 60, 'breakMinutes')}
+          btnUpId="break-increment"
+          onClickUp={this.handleIncDec(1, 1, 60, 'breakMinutes')}
+          valueId="break-length"
+          value={breakMinutes}
+        />
       </div>
     );
   }
